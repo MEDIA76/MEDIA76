edde45beb9682195b48b204d8bc0fb8630160104
Joshua Britt <joshua.britt@pm-international.com>
2019-02-13 13:29:36 -0600

    Added Google Analytics to site.

--- layouts/default.php
+++ layouts/default.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
     <title><?= TITLE; ?></title>
-    <link rel="shortcut icon" href="<?= path('/images/favicon.png'); ?>">
+    <link rel="shortcut icon" href="<?= path('/images/favicon.png'); ?>" />
     <link href="<?= path(['STYLES', 'default.css']); ?>" rel="stylesheet" />
+    <script async src="https://googletagmanager.com/gtag/js?id=UA-133296967-1"></script>
+    <script>window.dataLayer = window.dataLayer || [];function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'UA-133296967-1');</script>
   </head>
   <body>
     <section>
